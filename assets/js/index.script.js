aa6eecd032e5be32acfa221b436bf3e22ad73829
Alineevang <aline.evangelista@aluno.senai.br>
2023-08-29 09:31:04 -0300

    functions criadas, biblioteca, html

--- assets/js/index.script.js
+++ assets/js/index.script.js
@@ -48,3 +48,65 @@ class Pets {
 
 const petsTeste = new Pets ("Aline", "Marshall", "Pastor-alemão", "...", "12/04/2022");
 console.log(petsTeste);
+
+function cadastrarPets() {
+    let tutor = document.getElementById("input-tutor").value;
+    let nome = document.getElementById("input-nome").value;
+    let especie = document.getElementById("input-especie").value;
+    let foto = document.getElementById("input-fotinha").value;
+    let birthday = document.getElementById("input-birthday").value;
+
+    const pets = new Pets(tutor, nome, especie, foto, birthday);
+    console.log(pets);
+    bibliotecaPets.add(pets);
+    renderizarConteudo()
+}
+
+class ListaPets {
+    constructor() {
+        this.listaPets = [];
+    }
+    add(parametro){
+        if (verificarInputs()) {
+            envieMsg("Preencha todos os campos!", "erro");
+        }else {
+            this.listaPets.push(parametro);
+            limparInputs();
+            envieMsg("Cadastrado com sucesso!", "sucesso");
+            console.log(this.listaPets);
+        }
+    }
+}
+
+const bibliotecaPets = new ListaPets();
+console.log(bibliotecaPets);
+
+function limparInputs() {
+    let tutor = document.getElementById("input-tutor").value = '';
+    let nome = document.getElementById("input-nome").value = '';
+    let especie = document.getElementById("input-especie").value = '';
+    let foto = document.getElementById("input-fotinha").value = '';
+    let birthday = document.getElementById("input-birthday").value = '';
+}
+
+function renderizarConteudo() {
+    const listaHTML = document.getElementById('containerLista');
+    listaHTML.innerHTML = '';
+
+    let array = bibliotecaPets.listaPets;
+
+    console.log(array);
+
+    array.forEach(pet => {
+        const petDiv = `
+        <div class='petDetalhe'>
+        <h2>Título: ${pet.tutor}</h2>
+        <p>Nome: ${pet.nome}</p>
+        <p>Espécie: ${pet.especie}</p>
+        <p>Aniversário: ${pet.birthday}</p>
+        <img src="${pet.foto}" alt="${pet.titulo}">
+        `;
+
+        listaHTML.innerHTML += petDiv;
+    })
+}
